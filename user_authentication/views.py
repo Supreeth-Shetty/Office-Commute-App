6f2f37a202508156e31de4ad2b6a5400c2fe8d6e
Supreeth Shetty <shetty.supreeth95@gmail.com>
2022-05-16 15:12:37 +0530

    working on user autentication

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -1,8 +1,11 @@
 import os
 import re
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponse
 from logger.logger import Applogs
+from .models import User
+
 
 log = Applogs()
 log.getlogger(__file__)
@@ -17,7 +20,26 @@ def register(request):
 
 def register_employee(request):
     log.debug("routed to register_employee page")
-    return render(request, 'signup_employee.html')
+    if request.method == 'POST':
+        id = 1 
+        user_name = request.POST['User Name']
+        phone = request.POST['Phone']
+        email = request.POST['Email Id']
+        password = request.POST['password']
+        password_con = request.POST['confirm password']
+        company_id = int(request.POST['company_id'])
+        branch_id = int(request.POST['branch_id'])
+        todays_date = date.today().strftime("%d/%m/%Y")
+        print(user_name, phone, email, password, password_con, type(company_id))
+        users = User(
+            user_id=id, user_name=user_name, user_phone_number=phone, 
+            user_email_id=email, user_password=password, company_id=1, 
+            branch_id=1, user_type_id=1, created_date=todays_date, updated_date=todays_date, 
+            user_status='pending')
+        users.save()
+        return render(request, 'index.html')
+    else:
+        return render(request, 'signup_employee.html')
 
 def register_driver(request):
     log.debug("routed to register_driver page")
