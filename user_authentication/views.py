518171c886e7d0b61213d54f362e5b62e9fb1706
JaganathanRaja <jaganraja479@gmail.com>
2022-05-19 11:22:35 +0530

    login updated, employee and driver UI pages

--- user_authentication/views.py
+++ user_authentication/views.py
@@ -10,18 +10,21 @@ from .models import Users
 log = Applogs()
 log.getlogger(__file__)
 
+
 def homePageView(request):
     log.debug("landed on index page")
     return render(request, 'index.html')
+
 
 def register(request):
     log.debug("routed to ergister page")
     return render(request, 'register.html')
 
+
 def register_employee(request):
     log.debug("routed to register_employee page")
     if request.method == 'POST':
-        id = 1 
+        id = 1
         user_name = request.POST['User Name']
         phone = request.POST['Phone']
         email = request.POST['Email Id']
@@ -32,9 +35,9 @@ def register_employee(request):
         todays_date = date.today().strftime("%d/%m/%Y")
         print(user_name, phone, email, password, password_con, comp_id)
         users = Users(
-            user_id=id, user_name=user_name, user_phone_number=phone, 
-            user_email_id=email, user_password=password, company_id='AMZ', 
-            branch_id=branch_id, user_type_id=1, created_date=todays_date, updated_date=todays_date, 
+            user_id=id, user_name=user_name, user_phone_number=phone,
+            user_email_id=email, user_password=password, company_id='AMZ',
+            branch_id=branch_id, user_type_id=1, created_date=todays_date, updated_date=todays_date,
             user_status='pending')
         users.save()
         print(Users.objects.all())
@@ -42,10 +45,18 @@ def register_employee(request):
     else:
         return render(request, 'signup_employee.html')
 
+
 def register_driver(request):
     log.debug("routed to register_driver page")
-    return render(request, 'signup_driver.html')
-    
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'signup_driver.html')
+
+
 def login(request):
     log.debug("routed to login page")
-    return render(request, 'login.html')
+    if request.method == 'POST':
+        pass
+    else:
+        return render(request, 'login.html')
